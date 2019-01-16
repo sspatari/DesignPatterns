d33b02dd8234e7e7207bd64801ba43662f9d850a
sspatari <spatari.stanislav@gmail.com>
2019-01-16 06:16:52 +0200

    Change ruby each to my_each

--- lab3/test.rb
+++ lab3/test.rb
@@ -11,10 +11,8 @@ staff_names2 = %w[Cris Nacho]
 staff1 = staff_names1.map { |name| PersonnelFacade.new(name) }
 staff2 = staff_names2.map { |name| PersonnelFacade.new(name) }
 
-staff1.each { |worker| mercedes_suv.add_staff(worker) }
-staff2.each { |worker| huyndai_sedan.add_staff(worker) }
+Iterator.new(staff1).my_each { |worker| mercedes_suv.add_staff(worker) }
+Iterator.new(staff2).my_each { |worker| huyndai_sedan.add_staff(worker) }
 
 mercedes_suv.start_construction
 huyndai_sedan.start_construction
-
-Iterator.new([1, 2, 3]).my_each { |i| puts i }
